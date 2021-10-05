4de17727d06b319ffc5124326c25130cf39fc220
Kingcean Tuan <kingcean@live.com>
2021-10-05 22:27:01 +0800

    Update page content.

---
+++ static/js/config.js
@@ -0,0 +1,5 @@
+﻿(function () {
+    AliHub.Runtime.ModulexLoader.addModule("local", "../../inc/", true);
+    AliHub.Runtime.ModulexLoader.addModule("quark-panels", "https://g.alicdn.com/hub/quark-panels/0.5.42/");
+    if (!window.listener) window.listener = { push: function (str) { console.info(str); } };
+})();
