e50a8677853ba708c58baee4af3064e794f1355b
Kingcean Tuan <kingcean@live.com>
2021-10-10 17:32:38 +0800

    Add about page.

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,17 +1,17 @@
 window.pgCtrl = (function () {
     var menu = [
-        { name: "Blogs", url: "https://12dlabs-github-io.vercel.app" },
-        { name: "Games", url: "https://12dlabs.github.io/games" },
-        { name: "CompositeJs", url: "https://compositejs.github.io" }
+        { id: "blogs", name: "Blogs", url: "https://12dlabs-github-io.vercel.app" },
+        { id: "games", name: "Games", url: "https://12dlabs.github.io/games" },
+        { id: "about", name: "About", url: "https://12dlabs.github.io/about" }
     ];
 
-    function init () {
+    function init (branch) {
         var ele = document.createElement("footer");
         ele.innerHTML = "<section><div class=\"x-copyright\" style=\"color: transparent; \"><strong>12D</strong>Labs<br /></div></section>";
         document.body.appendChild(ele);
         ele = document.createElement("header");
         ele.innerHTML = "<section><h1><a href=\"https://12dlabs.github.io\"><strong>12D</strong><span>Labs</span></a></h1><ul>"
-            + menu.map(ele => "<li><a href=\"" + ele.url + "\">" + ele.name + "</a></li>").join("")
+            + menu.map(ele => "<li" + (branch === ele.id ? " class=\"state-sel\"" : "") + "><a href=\"" + ele.url + "\">" + ele.name + "</a></li>").join("")
             + "</ul></section>";
         document.body.insertBefore(ele, document.body.children[0]);
     };
